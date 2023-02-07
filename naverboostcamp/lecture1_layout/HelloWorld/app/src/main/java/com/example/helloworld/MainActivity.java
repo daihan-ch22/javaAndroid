79de4a39af59db4fd63c846b1ebd43900e6504dd
daihan-ch22 <daehan.dan.choi@gmail.com>
2023-02-07 09:13:49 +0900

    바꾸어보기2 done

--- naverboostcamp/lecture1_layout/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ naverboostcamp/lecture1_layout/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.helloworld;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(
+                getApplicationContext(), "버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void clickclick(View view){
+        Toast.makeText(getApplicationContext(), "moved", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.woodongs.site"));
+        startActivity(intent);
     }
 
 }
