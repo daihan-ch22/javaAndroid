e4e591caa46350fd5f6db20d92407b546861d996
daihan-ch22 <daehan.dan.choi@gmail.com>
2023-02-14 09:21:12 +0900

    parcelable / Serializable + ActivityResultLauncher사용

--- naverboostcourse/lecture3_components_variousScreens/myParcelable/app/src/main/java/org/dan/parcelable/MainActivity.java
+++ naverboostcourse/lecture3_components_variousScreens/myParcelable/app/src/main/java/org/dan/parcelable/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,13 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("data", data); //Menu로 던지기
 
 
-                startActivity(intent);
+                //startActivity(intent);
+                mStartForResult.launch(intent); //인텐트 전송
             }
         });
     }
 
+    //인텐트 응답 관련
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->
     {
+        if(result.getResultCode() == RESULT_OK){
+            Intent intent = result.getData();
 
+            //MenuActivity에서 넘어온 SimpleData 정보
+            // SimpleData 클래스는 Parcel객체에 담겨 전송된다.
+            SimpleData data = intent.getParcelableExtra("data");
+            String returnedMessage = data.message;
+
+
+            Log.i("--",returnedMessage);
+        }
     });
 }
--- naverboostcourse/lecture3_components_variousScreens/myParcelable/app/src/main/java/org/dan/parcelable/MenuActivity.java
+++ naverboostcourse/lecture3_components_variousScreens/myParcelable/app/src/main/java/org/dan/parcelable/MenuActivity.java
@@ -29,6 +29,7 @@ public class MenuActivity extends AppCompatActivity {
 
 
         Intent passedIntentFromMain = getIntent();
+        setResult(RESULT_OK, passedIntentFromMain);
         processIntent(passedIntentFromMain);
     }
 
@@ -36,11 +37,14 @@ public class MenuActivity extends AppCompatActivity {
         if(intent != null) {
             //ArrayList는 자바 기본 API이고 Serialize 인터페이스를 구현했기 때문에 사용 가능
             ArrayList<String> names = (ArrayList<String>) intent.getSerializableExtra("names");
-            if(names != null)
-            Toast.makeText(getApplicationContext(), "전달받은 이름 리스트 갯수: " + names.size(), Toast.LENGTH_SHORT).show();
+
+            if(names != null) {
+                Toast.makeText(getApplicationContext(), "전달받은 이름 리스트 갯수: " + names.size(), Toast.LENGTH_SHORT).show();
+            }
 
             SimpleData data = (SimpleData) intent.getParcelableExtra("data");
             if(data != null){
+
                 Toast.makeText(getApplicationContext(), "전달받은 SimpleData: " + data.number + " " + data.message , Toast.LENGTH_SHORT).show();
             }
         }
