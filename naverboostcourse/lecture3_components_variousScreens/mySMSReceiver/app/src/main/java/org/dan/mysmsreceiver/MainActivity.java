ee5e386122c05f26618bd82c6574c80912842550
daihan-ch22 <daehan.dan.choi@gmail.com>
2023-02-17 12:36:06 +0900

    Permission Request done

--- naverboostcourse/lecture3_components_variousScreens/mySMSReceiver/app/src/main/java/org/dan/mysmsreceiver/MainActivity.java
+++ naverboostcourse/lecture3_components_variousScreens/mySMSReceiver/app/src/main/java/org/dan/mysmsreceiver/MainActivity.java
@@ -1,8 +1,16 @@
 package org.dan.mysmsreceiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);//권한 체크
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Granteed Permission of Receiving SMS", Toast.LENGTH_LONG).show();
+        } else {
+
+            Toast.makeText(getApplicationContext(), "No Permission has granteed.", Toast.LENGTH_LONG).show();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)) //권한에 대한 설명이 필요한지??
+            {
+                Toast.makeText(this, "Required SMS Receive Permission.", Toast.LENGTH_LONG).show();
+            } else {
+                //권한 달라고 요청
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 101);
+            }
+        }
+    }
+
+    //권한 부여후 결과를 얻기 위한 콜백함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 101:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "SMS Permission Granted by User :)", Toast.LENGTH_LONG).show();
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "SMS Permission Denied by User :(", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, "SMS Permission has been rejected :/", Toast.LENGTH_LONG).show();
+                    }
+                }
+        }
     }
 }
--- wrappingUp.md
+++ wrappingUp.md
@@ -1,7 +1,4 @@
-# KEYWORDS
-
-
-
+KEYWORDS
 
 <br>
 
@@ -9,6 +6,8 @@
 
 
 <br><br>
+
+----
 
 
 ## EVENT HANDLING
@@ -22,6 +21,7 @@
 
 <br><br>
 
+----
 
 ## INFLATION 
 
@@ -30,6 +30,7 @@ foo.xml ------ CONNECT ------ foo_activity.java
 
 * setContentView() / getSystemService(Context.LAYOUT_INFLATER_SERVICE)
 
+----
 
 <br><br>
 
@@ -42,13 +43,13 @@ foo.xml ------ CONNECT ------ foo_activity.java
 * Adapter
 
 
-
+-----
 
 
 
 <br><br><br>
 
-## 4대 구성요소 + Intent
+# 4대 구성요소 + Intent
 
 <br>
 
@@ -63,7 +64,59 @@ foo.xml ------ CONNECT ------ foo_activity.java
 StartActivity / StartActivityResult
 
 ### Service
+
+* startService() / onCreate() / onDestroy()
+* UI가 없다(안보임)
+
 ### Broadcast Receiver
+* 전화나 SMS메시지, 채팅 (전화 / 채팅이 왔습니다 == Global Event를 처리하기 위함) 
+* UI가 없다(안보임)
+* manifest에 등록을 해야 사용 가능 
+* start activity/service처럼 따로 시작을 할 필요가 없음 -> manifest에 등록해놓으면 된다. 
+
+```xml
+<receiver
+            android:name=".SMSReceiver"
+            android:enabled="true"
+            android:exported="true">
+            
+            <intent-filter> //인텐트 이것만 사용하겠다 
+                <action android:name="android.provider.Telephony.SMS_RECEIVED"/>
+            </intent-filter>
+            
+        </receiver>
+```
+```java 정형화된 문자 관련 리시버 코드
+
+private SmsMessage[] parseSmsMessage(Bundle bundle){
+        Object[] objs = (Object[])bundle.get("pdus");
+        SmsMessage[] messages = new SmsMessage[objs.length];
+
+        for(int i=0;i<objs.length;i++){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[])objs[i], format);
+            }
+            else{
+                messages[i] = SmsMessage.createFromPdu((byte[])objs[i]);
+            }
+
+        }
+        return messages;
+    }
+
+```
+
+* Broadcast는 2가지로 나뉨
+  * [일반 브로드캐스트] - sendBroadcase()로 호출
+    * 비동기 실행 / 모든 수신자는 순서 없이 실행(때로는 동시에)
+    * 효율적이지만 한 수신자의 처리 결과를 다른 수신자가 이용할 수 없고 중도 취소 불가
+
+  * [순차 브로드캐스트] - sendOrderedBroadcase()로 호출
+    * 한번에 하나의 수신자에만 전달 - 순서대로 진행된다.
+    * 중간에 취소하면 다음 수신자는 받지 못한다. 
+    * 순서는 Intent Filter 속성으로 정할 수 있고 순서가 같으면 랜덤 실행 
+
 ### Content Provider
 
 ### Intent
